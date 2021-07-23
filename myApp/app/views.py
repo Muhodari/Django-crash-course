975e72fa76a62b8e9c75a0cde51bb41a36a3a26f
Muhodari Sage <sagemuho@gmail.com>
2021-07-23 10:41:21 +0200

    added sign up

--- myApp/app/views.py
+++ myApp/app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Feature
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 
 # Create your views here.
@@ -16,6 +18,33 @@ def index(request):
     features = Feature.objects.all()
 
     return render(request, 'static.html', {'features': features})
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('register')
+
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Used')
+                return redirect('register')
+
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                redirect('login')
+        else:
+            messages.info(request, 'Password Not the same')
+            redirect('register')
+    else:
+        return render(request, 'register.html')
 
 
 def counter(request):
