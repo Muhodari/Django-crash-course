ac522d8d7a1f1aefbd08cc2d8ac7a98fb6f2c4c7
Muhodari Sage <sagemuho@gmail.com>
2021-07-23 02:54:20 +0200

    added word counter

--- myApp/app/views.py
+++ myApp/app/views.py
@@ -11,3 +11,9 @@ def index(request):
         'Nationality': 'Rwanda'
     }
     return render(request, 'index.html', context)
+
+
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount':amount_of_words})
