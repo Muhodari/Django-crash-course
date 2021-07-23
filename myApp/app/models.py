4adde66e50294d175ddf86e0c527f441d2a9ca86
Muhodari Sage <sagemuho@gmail.com>
2021-07-23 09:01:40 +0200

    get data from database to html file

--- myApp/app/models.py
+++ myApp/app/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 
 # Create your models here.
-class Feature:
-    id: int
-    name: str
-    details: str
-    is_true: bool
+# normal declaration of class
 
+# class Feature:
+#     id: int
+#     name: str
+#     details: str
+#     is_true: bool
+
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    detail = models.CharField(max_length=500)
 
--- myApp/app/views.py
+++ myApp/app/views.py
@@ -12,35 +12,10 @@ def index(request):
         'Nationality': 'Rwanda'
     }
     # return render(request, 'index.html', context)
+    # getting data from database
+    features = Feature.objects.all()
 
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.is_true=True
-    feature1.details = 'our services are very quick'
-
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Reliable'
-    feature2.is_true=True
-    feature2.details = 'our services are very Reliable'
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Easy to use'
-    feature3.is_true = False
-    feature3.details = 'our services are Easy to  use '
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Affordable'
-    feature4.is_true=True
-    feature4.details = 'our services are very affordable'
-
-    features = [feature1, feature2, feature3, feature4]
-
-    return render(request, 'static.html',
-                  {'features': features})
+    return render(request, 'static.html', {'features': features})
 
 
 def counter(request):
