d4322e422f626794d65c00fd5c0890ae18aab116
Muhodari Sage <sagemuho@gmail.com>
2021-07-23 03:04:02 +0200

    added sending data with post on word counter for security

--- myApp/app/views.py
+++ myApp/app/views.py
@@ -14,6 +14,7 @@ def index(request):
 
 
 def counter(request):
-    text = request.GET['text']
+    # text = request.GET['text']
+    text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount':amount_of_words})
