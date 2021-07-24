e0d2172d0f61184de7e865005947cde464f5e329
Muhodari Sage <sagemuho@gmail.com>
2021-07-24 12:34:29 +0200

    added connection to database postgres and finally  done with django crash course

--- myApp/myApp/settings.py
+++ myApp/myApp/settings.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = 'myApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_project',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+
     }
 }
 
